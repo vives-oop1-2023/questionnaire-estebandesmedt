adc7cb89c5e2fe78dece47c19d92b3b91d9e251f
Esteban Desmedt <estebandesmedt33@gmail.com>
2023-05-26 17:13:35 +0200

    p

--- Questionnare/Login.xaml.cs
+++ Questionnare/Login.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Questions;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,6 +12,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using TriviaApiLibrary;
 
 namespace Questionnare
 {
@@ -20,6 +22,7 @@ namespace Questionnare
     public partial class Login : Window
     {
         private int maxQuestion = 12;
+        Question question1;
         public Login()
         {
             InitializeComponent();
@@ -43,5 +46,7 @@ namespace Questionnare
             about.Show();
             this.Close();
         }
-    }
+
+    }    
 }
+
